8b503981654c4290f8fdede69d9b461384e5d5e7
helino <erik.helin@gmail.com>
2011-02-25 10:49:44 +0100

    ported ast_lists to the new framework

--- tests/ast/ast_tests.h
+++ tests/ast/ast_tests.h
@@ -3,30 +3,30 @@
 
 CuSuite* ast_test_identifier();
 CuSuite* ast_test_ast_walk();
-CuSuite* test_binary_operation();
 CuSuite* ast_test_integer();
 CuSuite* ast_test_unary_operation();
 CuSuite* ast_test_boolean();
 CuSuite* ast_test_new_object();
 CuSuite* ast_test_this();
-CuSuite* ast_test_ast_list();
 CuSuite* ast_test_call();
 CuSuite* ast_test_print();
 CuSuite* ast_test_main_class();
 
 CuSuite *ast_tests()
 {
+    CuSuite* test_ast_list();
+    CuSuite* test_binary_operation();
     CuSuite* ast_tests = CuSuiteNew();
 
+    CuSuiteAddSuite(ast_tests, test_ast_list());
+    CuSuiteAddSuite(ast_tests, test_binary_operation());
     CuSuiteAddSuite(ast_tests, ast_test_identifier());
     CuSuiteAddSuite(ast_tests, ast_test_ast_walk());
-    CuSuiteAddSuite(ast_tests, test_binary_operation());
     CuSuiteAddSuite(ast_tests, ast_test_integer());
     CuSuiteAddSuite(ast_tests, ast_test_unary_operation());
     CuSuiteAddSuite(ast_tests, ast_test_boolean());
     CuSuiteAddSuite(ast_tests, ast_test_new_object());
     CuSuiteAddSuite(ast_tests, ast_test_this());
-    CuSuiteAddSuite(ast_tests, ast_test_ast_list());
     CuSuiteAddSuite(ast_tests, ast_test_call());
     CuSuiteAddSuite(ast_tests, ast_test_print());
     CuSuiteAddSuite(ast_tests, ast_test_main_class());
--- tests/ast/test_ast_list.c
+++ tests/ast/test_ast_list.c
@@ -1,7 +1,5 @@
-#include "CuTest.h"
+#include "spectacular.h"
 #include "ast.h"
-#include "stddef.h"
-#include "stdlib.h"
 #include "list.h"
 
 static ast_list* create(list *list)
@@ -9,31 +7,27 @@ static ast_list* create(list *list)
     return (ast_list *) new_ast_list(list);
 }
 
-void should_have_ast_list_as_type(CuTest *tc)
-{
+begin_spec(ast_list, should_have_ast_list_as_type)
     list *list = new_list();
     ast_list *al = create(list);
-    CuAssertIntEquals(tc, AST_LIST, al->type);
+
+    should_eq_int(AST_LIST, al->type);
+
     free(al);
     free(list);
-}
+end_spec
 
-void should_have_the_given_list_as_member(CuTest *tc)
-{
+begin_spec(ast_list, should_have_the_given_list_as_member)
     list *list = new_list();
     ast_list *al = create(list);
-    CuAssertPtrEquals(tc, list, al->list);
+
+    should_eq_ptr(list, al->list);
+
     free(al);
     free(list);
-}
+end_spec
 
-CuSuite* ast_test_ast_list()
-{
-    CuSuite *ast_list = CuSuiteNew();
-    
-    SUITE_ADD_TEST(ast_list, should_have_ast_list_as_type);
-    SUITE_ADD_TEST(ast_list, should_have_the_given_list_as_member);
-
-    return ast_list;
-}
-
+begin_suite(ast_list)
+    add_spec(should_have_ast_list_as_type)
+    add_spec(should_have_the_given_list_as_member)
+end_suite
