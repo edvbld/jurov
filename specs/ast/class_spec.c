39a687b538d91a6bae01805ca99f7cae97c3425b
helino <erik.helin@gmail.com>
2011-03-12 17:16:26 +0100

    added a negative example

--- specs/ast/class_spec.c
+++ specs/ast/class_spec.c
@@ -30,7 +30,27 @@ begin_example(mj_class, should_have_the_given_parameters_as_members)
     free(c);
 end_example
 
+begin_example(mj_class, should_return_error_when_given_bad_type)
+    mj_class *c;
+    ast *num;
+
+    should_pass(new_mj_integer(6, &num))
+    should_eq_error(JRV_INVALID_TYPE, 
+                    new_mj_class(num, NULL, NULL, (ast **) &c))
+    should_eq_ptr(NULL, c)
+    should_eq_error(JRV_INVALID_TYPE, 
+                    new_mj_class(NULL, num, NULL, (ast **) &c))
+    should_eq_ptr(NULL, c)
+    should_eq_error(JRV_INVALID_TYPE, 
+                    new_mj_class(NULL, NULL, num, (ast **) &c))
+    should_eq_ptr(NULL, c)
+
+    free(num);
+    free(c);
+end_example
+
 begin_description(mj_class)
     add_example(should_have_mj_class_as_type)
     add_example(should_have_the_given_parameters_as_members)
+    add_example(should_return_error_when_given_bad_type)
 end_description
